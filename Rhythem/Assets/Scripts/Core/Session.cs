3c8f17337a1dc733e0476f58f1c135044e0e7f94
delton <hulbert.delton@gmail.com>
2025-01-25 03:20:04 -0500

    adding various singletons for project structure and cleanliness

---
+++ Rhythem/Assets/Scripts/Core/Session.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+namespace Rhythem.Core
+{
+    /// <summary>
+    /// Sessions are the main overall gameplay states of the game. These sessions
+    /// can include things like a Splash Session, a Play Session, etc.
+    /// </summary>
+    public abstract class Session<T> : Session where T : Session<T>
+    {
+        public static T Instance { get; private set; }
+
+        protected override void Awake()
+        {
+            Instance = (T)this;
+            base.Awake();
+        }
+    }
+
+    public class Session : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+
+        }
+
+        /// <summary>
+        /// Handle any initialization needed for this mode.
+        /// </summary>
+        public virtual void Initialize()
+        {
+
+        }
+
+        /// <summary>
+        /// WARNING: Should not be called outside of ModeManager. Removes the mode, cleaning itself up.
+        /// </summary>
+        public virtual void EndSession()
+        {
+
+        }
+
+        /// <summary>
+        /// WARNING: Should not be called outside of ModeManager. Trigger the pause or unpause functionality needed for this mode.
+        /// </summary>
+        public virtual void Pause(bool paused)
+        {
+
+        }
+    }
+}
